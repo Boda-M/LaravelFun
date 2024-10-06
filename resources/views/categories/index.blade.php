adbe0dcccd5fe5a7244e109d561d02c4df356912
Boda-M <mateboda05@gmail.com>
2024-10-06 16:45:47 +0200

    Buttons

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -12,7 +12,16 @@
 
 <ul>
     @foreach($categories as $category)
-    <li>{{$category->id}} - {{ $category->name}}</li>
+    <li>
+        {{ $category->name}}
+        <a href="{{ route('categories.edit', $category->id)}}" class="button">Szerkesztés</a>
+        <a href="{{ route('categories.show', $category->id)}}" class="button">Megjelenítés</a>
+        <form action="{{ route('categories.destroy', $category->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="danger">Törlés</button>
+        </form>
+    </li>
     @endforeach
 </ul>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,14 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AII eszközök listája</title>
+    <title>AI eszközök listája</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&family=Quicksand&display=swap">
     <link rel="stylesheet" href="{{asset('style.css')}}">
+    <link rel="shortcut icon" href="{{asset('favicon.ico')}}" type="image/x-icon">
 </head>
 <body>
-    <header></header>
+    <header>
+        <img src="{{asset('logo.png')}}" alt="logo">
+        <nav>
+            <ul>
+                <li><a href="{{route('categories.index')}}">Kategóriák</a></li>
+                <li><a href="{{route('categories.create')}}">Új kategória</a></li>
+            </ul>
+        </nav>
+    </header>
     <main>
     @yield('content')
     </main>
-    <footer></footer>
+    <footer><p>&copy 2024</p></footer>
 </body>
 </html>
